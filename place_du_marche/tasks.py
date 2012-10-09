d96d13eb0e5c82e6cb8ef6416754f1bb52cdb0a6
Ahmed Boussadia <ahmed.boussadia@gmail.com>
2012-10-09 11:11:58 +0200

    Excluding tests

--- place_du_marche/tasks.py
+++ place_du_marche/tasks.py
@@ -116,5 +116,5 @@ def get_place_du_marche_categories():
 					print "Product did not change"
 
 
-get_place_du_marche_categories.delay()
+# get_place_du_marche_categories.delay()
 # print place_du_marche.extract_product("http://www.placedumarche.fr/supermarche-en-ligne-livraison-tendre-noix-la-broche-2-1-tranche-gratuite,10179,4,182,1001.htm")
