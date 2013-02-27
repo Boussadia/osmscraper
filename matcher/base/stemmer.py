9b8e07a02445ad7fd1246391bf0240663b51aafc
Ahmed Boussadia <ahmed.boussadia@gmail.com>
2013-02-27 11:45:03 +0100

    Improving stemmer

--- matcher/base/stemmer.py
+++ matcher/base/stemmer.py
@@ -5,22 +5,48 @@ import re
 import time
 
 from bs4 import BeautifulSoup, Comment
-import Stemmer
 
 from ooshop.models import NewProduct as Product
 from matcher.models import BaseWord, Stem
 from osmscraper.unaccent import unaccent
 
-class BaseHTMLStemmer(object):
+class Stemmer(object):
+	"""
+		This class is responsible for stemming a sentence.
+	"""
+	def __init__(self, text = ''):
+		self.text = text
+		self.stemmed_text = None
+
+	def stem_text(self):
+		"""
+			This method is responsible for generating a text with words replaced by stems.
+		"""
+		text = self.text.lower()
+		stems = []
+		words = re.split(r'[^a-zA-Z0-9]+', unaccent(text))
+		words = tuple([str(w) for w in words])
+		new_words = []
+		base_words = { unaccent(bw.text.lower()): unaccent(bw.stem.word.lower()) for bw in BaseWord.objects.raw("select * from matcher_baseword where unaccent(lower(matcher_baseword.text)) in %s", [words])}
+		# Now we need to go through the base words and replace found words by stems in text
+		for word in words:
+			if word in base_words:
+				new_words.append(base_words[word])
+			else:
+				new_words.append(word)
+
+		# Reconstucting text and returning it
+		return ' '.join(new_words)
+
+class BaseHTMLStemmer(Stemmer):
 	"""
 		This class is responsible for stemming a sentence. It takes a html formated string, converts it to 
 		a soup object, extract text the text and stems it.
 	"""
 	def __init__(self, html):
+		super(BaseHTMLStemmer, self).__init__()
 		self.html = html
 		self.soup = BeautifulSoup(html, "lxml", from_encoding = 'utf-8')
-		self.text = None
-		self.stemmed_text = None
 
 	def extract_text(self):
 		"""
@@ -38,7 +64,7 @@ class BaseHTMLStemmer(object):
 		"""
 			This method is responsible for generating a text with words replaced by stems.
 		"""
-		if self.text is None:
+		if self.text is None or self.text == '':
 			self.extract_text()
 		text = self.text.lower()
 		stems = []
@@ -55,4 +81,3 @@ class BaseHTMLStemmer(object):
 
 		# Reconstucting text and returning it
 		return ' '.join(new_words)
-		
