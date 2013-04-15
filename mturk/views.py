8fe21f60321aca53416a41b5bef5df918d76df98
Ahmed Boussadia <ahmed.boussadia@gmail.com>
2013-04-15 16:53:39 +0200

    Changing front end for test view in mturk

--- mturk/views.py
+++ mturk/views.py
@@ -1,14 +1,30 @@
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.db import connection, transaction
-from django.db.models import Q
+
+from scrapers.base.basecrawler import BaseCrawler as Crawler
+
+
 
 def test(request):
 	response = {}
 	if request.method == 'GET':
 		if 'assignmentId' in request.GET:
 			assignmentId = request.GET['assignmentId']
+			hitId = request.GET['hitId']
 		else:
 			assignmentId = None
+			hitId = None
 		response['assignmentId'] = assignmentId
+		response['hitId'] = hitId
+
+	if request.method == 'POST':
+		assignmentId = request.POST['assignmentId']
+		hitId = request.POST['hitId']
+		response['assignmentId'] = assignmentId
+		response['hitId'] = hitId
+
+		crawler = Crawler()
+		# Posting data to amazon mturk
+		crawler.post(url = 'https://www.mturk.com/mturk/externalSubmit', data = response)
+
 	return render(request, 'mturk/test.html', response)
