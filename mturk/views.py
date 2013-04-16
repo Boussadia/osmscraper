a7014588783cfce8ac5607ea68ac5b42f993e00d
Ahmed Boussadia <ahmed.boussadia@gmail.com>
2013-04-16 09:03:52 +0200

    Removing logs and test in order to raise error and read trace

--- mturk/views.py
+++ mturk/views.py
@@ -1,9 +1,3 @@
-# import the logging library
-import logging
-
-# Get an instance of a logger
-logger = logging.getLogger(__name__)
-
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
@@ -37,19 +31,14 @@ def index(request, key):
 		if 'turkSubmitTo' in parameters:
 			response['turkSubmitTo'] = unicode(parameters['turkSubmitTo']) + '/mturk/externalSubmit'
 
-	logger.info('URl Submit To : %s'%(str(response['turkSubmitTo'])))
-
 	helper = MturkHelper(key = key)
 	response.update(helper.dump())
 
 	if request.method == 'POST':
 		reference_result = request.POST['flagged']
 		helper.save_result(reference_result, response['hitId'], response['assignmentId'], response['workerId'])
-		if response['turkSubmitTo'] is not None:
-			crawler = Crawler()
-			# Posting data to amazon mturk
-			r, c = crawler.post(url = response['turkSubmitTo'], data = response)
-			logging.info('Response to post (%d) :'%c)
-			logging.info(r)
+		crawler = Crawler()
+		# Posting data to amazon mturk
+		crawler.post(url = response['turkSubmitTo'], data = response)
 
 	return render(request, 'mturk/index.html', response)
