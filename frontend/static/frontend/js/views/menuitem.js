68d99727520cc3a91f26fc6af6ab6cb0b079f1b5
Ahmed Boussadia <ahmed.boussadia@gmail.com>
2013-04-23 18:19:23 +0200

    Working on menu style

--- frontend/static/frontend/js/views/menuitem.js
+++ frontend/static/frontend/js/views/menuitem.js
@@ -5,17 +5,17 @@ define([
 		var MenuItemView = BaseView.extend({
 			tagName: 'li',
 			className: 'has-dropdown',
-			template: menuItemTemplate,
+			template: _.template(menuItemTemplate),
 			initialize: function(options){
 				options || (options = {});
 				this.model = options.model || {};
 			},
 			render: function(){
 				this.$el.empty();
-				var template = _.template(this.template);
 				var data = this.model.toJSON();
-				this.$el.append(template(data));
+				this.$el.append(this.template(data));
 				return this;
+
 			},
 
 			events:{
