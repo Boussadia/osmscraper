00e1958450c4adacc715ceb9034d3b02eaf3874c
Ahmed Boussadia <ahmed.boussadia@gmail.com>
2013-04-24 15:21:27 +0200

    Adding plus button

--- frontend/static/frontend/js/views/products.js
+++ frontend/static/frontend/js/views/products.js
@@ -3,14 +3,15 @@ define([
 	'collections/products',
 	'views/base',
 	'views/product',
-	'text!../../templates/products.html'
+	'text!../../templates/products.html',
+	'text!../../templates/plus.html'
 	],
-	function(_, ProductsCollection, BaseView, ProductView, productsTemplate){
+	function(_, ProductsCollection, BaseView, ProductView, productsTemplate, plusTemplate){
 
 		var ProductsView = BaseView.extend({
 			tagName:'div',
 			className: 'products',
-			template: productsTemplate,
+			template: _.template(productsTemplate),
 			initialize: function(options){
 				options || (options = {});
 				this.products = options.products || new ProductsCollection([], {'vent': this.vent});
@@ -22,15 +23,16 @@ define([
 			render: function(){
 				this.closeSubViews();
 				this.$el.empty();
-
-				var template = _.template(this.template);
 				var data = {'name': this.products.name, 'count': this.products.count};
-				this.$el.append(template(data));
+				this.$el.append(this.template(data));
 				var that = this;
 				this.products.each(function(product){
 					var view = new ProductView({'product': product, 'vent': this.vent})
 					that.$el.append(view.render().el);
 				})
+
+				var plus = _.template(plusTemplate)();
+				this.$el.append(plus);
 
 				return this;
 			}
