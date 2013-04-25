7cc9305e7229b42b30de3f34d863d02a71d5e80b
Ahmed Boussadia <ahmed.boussadia@gmail.com>
2013-04-25 19:09:46 +0200

    Workig on style of product blocs

--- frontend/static/frontend/js/views/product.js
+++ frontend/static/frontend/js/views/product.js
@@ -5,6 +5,8 @@ define([
 	], function(ProductModel, BaseView, productTemplate){
 
 		var ProductView = BaseView.extend({
+			// maximum length of product name,
+			MAX_NAME_LENGTH: 142,
 			tagName: 'div',
 			className: 'product',
 			model: ProductModel,
@@ -16,6 +18,10 @@ define([
 			render: function(){
 				var template = _.template(this.template);
 				var data = this.product.toJSON();
+				if (data.name.length > this.MAX_NAME_LENGTH){
+					console.log(data.name.length);
+					data.name = data.name.substring(0, this.MAX_NAME_LENGTH-3)+'...';
+				}
 				this.$el.append(template(data));
 				return this;
 			}
