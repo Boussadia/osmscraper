4cf9258fcae57618525a96c25d6ff6708648c5a7
Ahmed Boussadia <ahmed.boussadia@gmail.com>
2013-05-01 11:19:04 +0200

    Automatic redirection to a set category when accessing root of app

--- frontend/static/frontend/js/router.js
+++ frontend/static/frontend/js/router.js
@@ -3,12 +3,17 @@ define([
 ], function(Backbone){
 	Router = Backbone.Router.extend({
 		routes: {
+			'': 'index',
 			'categorie/:parentCategoryName/:childCategoryName': 'category',
 			'*any':'any'
 		},
 		initialize: function(options){
 			options || (options = {})
 			this.vent = options.vent || null;
+		},
+		index: function(){
+			var url = '/categorie/epicerie-sucree/cafes-et-chicorees'
+			this.navigate(url, true);
 		},
 		category: function(parentCategoryName, childCategoryName){
 			var url = parentCategoryName+'/'+childCategoryName;
