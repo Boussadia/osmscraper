5af4a07f8c3bdcc0cdd5e1dc5ca652fd478cf498
Ahmed Boussadia <ahmed.boussadia@gmail.com>
2013-06-06 15:49:48 +0200

    Adding loader for osms, #7

--- apps/frontend/static/frontend/js/views/osms.js
+++ apps/frontend/static/frontend/js/views/osms.js
@@ -11,6 +11,16 @@ define([
 			TRIGGER: 138,
 			initialize: function(options){
 				this.osms = options.osms || new OsmsCollection([], {'vent': this.vent});
+				var that = this;
+				this.bindTo(this.osms, 'request', function(){
+					that.$el.find('.loader').show();
+					that.$el.find('.loader+p').hide();
+				});
+
+				this.bindTo(this.osms, 'sync', function(){
+					that.$el.find('.loader').hide();
+					that.$el.find('.loader+p').show();
+				});
 
 				this.bindTo(this.osms, 'change', this.render);
 			},
