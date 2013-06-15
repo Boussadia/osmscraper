66d32ab9db4bf70e1a4342d888a68e5678211811
Ahmed Boussadia <ahmed.boussadia@gmail.com>
2013-06-15 12:02:35 +0200

    Removing deprecated code

--- apps/frontend/static/frontend/js/collections/base.js
+++ apps/frontend/static/frontend/js/collections/base.js
@@ -8,11 +8,6 @@ define([
 			this.vent = options.vent || {};
 			var that = this;
 
-			this.on('sync', function(model, resp, options){
-				if ( (typeof that.vent !== 'undefined') && that.vent.hasOwnProperty('trigger') && (typeof resp !== 'undefined') && (typeof resp.carts !== 'undefined')) that.vent.trigger('carts', resp.carts);
-				
-			});
-
 			this.on('error', function(model, resp, options ){
 				if((resp.status === 403) && (typeof model !== 'undefined') && (typeof model.vent !== 'undefined') && (typeof model.vent.trigger !== 'undefined')) model.vent.trigger('user:403');
 			})
