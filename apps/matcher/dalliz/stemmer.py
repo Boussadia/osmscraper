d1ae1a0c14bd7601691be38ede71b8d51df7f458
Ahmed Boussadia <ahmed.boussadia@gmail.com>
2013-06-26 10:12:16 +0200

    Adding celery task for stemmer

--- apps/matcher/dalliz/stemmer.py
+++ apps/matcher/dalliz/stemmer.py
@@ -2,6 +2,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import # Import because of modules names
 
+from django.conf import settings
+from celery import Celery
+from celery.task import periodic_task, task
+
 from apps.matcher.auchan.auchanstemmer import AuchanStemmer
 from apps.matcher.monoprix.monoprixstemmer import MonoprixStemmer
 from apps.matcher.ooshop.ooshopstemmer import OoshopStemmer
@@ -34,7 +38,20 @@ class DallizStemmer(object):
 			Stemmer = globals()[stemmer_class_name]
 
 			products = OSMProduct.objects.filter(html__isnull = False)
+			count = products.count()
+			i = 0
 
 			for product in products:
+				i = i+1
+				progress = 100
+				if count>0:
+					progress = round(i*10000/count)/100.0
+				print 'Stemming %s (Progress : %f %s)'%(product.name, progress, '%')
 				product.stemmed_text = Stemmer(product.html).stem_text()
 				product.save()
+
+celery = Celery('tasks', broker=settings.BROKER_URL)
+
+@celery.task
+def run_stemmer():
+	DallizStemmer.run()
